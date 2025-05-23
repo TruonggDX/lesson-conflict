fc52de2150ee7f9b6e54046afb385ad2eeb2048a
TruonggDX <dinhxuantruong2003coi@gmail.com>
2025-05-23 14:27:12 +0700

    first commit

---
+++ src/main/java/com/exo/resolveconflict/ResolveConflictApplication.java
@@ -0,0 +1,13 @@
+package com.exo.resolveconflict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveConflictApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ResolveConflictApplication.class, args);
+  }
+
+}
