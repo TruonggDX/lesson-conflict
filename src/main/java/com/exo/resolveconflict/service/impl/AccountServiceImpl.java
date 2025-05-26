e58546881c9f821a6f00827d1899660d207d31d1
TruonggDX <dinhxuantruong2003coi@gmail.com>
2025-05-26 10:02:05 +0700

    change file

--- src/main/java/com/exo/resolveconflict/service/impl/AccountServiceImpl.java
+++ src/main/java/com/exo/resolveconflict/service/impl/AccountServiceImpl.java
@@ -19,7 +19,7 @@ public class AccountServiceImpl implements IAccountService {
 
   @Override
   public List<AccountEntity> findAll() {
-    log.info("Find all accounts hehe {}", accountRepository.findAll());
+    log.info("Find all accounts hehe kaakak {}", accountRepository.findAll());
     return accountRepository.findAll();
   }
 
